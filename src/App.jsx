1f6486c4d1d0f16cb9cf8751599dd4896d9d5cc4
nihal0814 <nihal.rahangdale24@vit.edu>
2026-02-10 18:45:41 +0530

    admin console banaya

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import "./App.css";
 import DashboardCards from "./components/DashboardCards";
 import ProductTable from "./components/ProductTable";
 import AddProductModal from "./components/AddProductModal";
+import AdminPage from "./components/AdminPage";
+import Navbar from "./components/Navbar"; // 1. Import Navbar
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [showAdmin, setShowAdmin] = useState(false);
 
   const addProduct = (product) => {
     setProducts([...products, product]);
@@ -17,11 +20,18 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <div className="app-title">MAHALE INDUSTRIES</div>
+      {/* 2. Add Navbar Component */}
+      <Navbar setShowAdmin={setShowAdmin} />
 
-      <DashboardCards products={products} />
-      <AddProductModal addProduct={addProduct} />
-      <ProductTable products={products} deleteProduct={deleteProduct} />
+      {showAdmin ? (
+        <AdminPage onClose={() => setShowAdmin(false)} />
+      ) : (
+        <>
+          <DashboardCards products={products} />
+          <AddProductModal addProduct={addProduct} />
+          <ProductTable products={products} deleteProduct={deleteProduct} />
+        </>
+      )}
     </div>
   );
 };
